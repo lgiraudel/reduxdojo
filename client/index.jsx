4998280345e91f95b99f02b61c9641c24b4894a8
Loïc Giraudel <lgiraudel@purch.com>
2015-12-07 23:47:00 +0100

    Plug logger to see actions

--- client/index.jsx
+++ client/index.jsx
@@ -4,8 +4,15 @@ import App from './components/App.jsx';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducers from './reducers/reducers';
+import createLogger from 'redux-logger';
+import { compose, applyMiddleware } from 'redux';
 
-var store = createStore(reducers);
+const loggerMiddleware = createLogger();
+var createStoreWithMiddleware = compose(
+    applyMiddleware(loggerMiddleware)
+)(createStore);
+
+var store = createStoreWithMiddleware(reducers);
 
 render(
     <Provider store={store}>
